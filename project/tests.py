b3d787652b124cf0e28750ebc7bbfe9a74fdf91c
Afraaz1 <46198568+Afraaz1@users.noreply.github.com>
2021-11-13 15:15:20 +0000

    tests

--- project/tests.py
+++ project/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from . models import Post
 
 # Create your tests here.
+class ModelTesting(TestCase):
+
+    def setUp(self): 
+        self.project = Post.objects.create(title='django', author='django', slug='django')
+
+    def test_post_model(self):
+        d = self.project
+        self.assertTrue(isinstance(d, Post))
+        self.assertEqual(str(d), 'django')
